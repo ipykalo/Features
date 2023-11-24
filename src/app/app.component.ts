a260a9d7469d2dc7a58285a68bfdf2703bb69bf2
Ivan <ipykaloi@gmail.com>
2023-11-25 00:01:43 +0200

    added home & header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle"
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    RouterLink,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
